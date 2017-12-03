a38bef1c336ded393e9993a89b28248836342080
anmolg1024 <sam.amlgpt@gmail.com>
2017-12-03 19:05:33 +0530

    Mohit

--- pipeline_datapath.vhd
+++ pipeline_datapath.vhd
@@ -77,6 +77,7 @@ end component;
 
 component lm_sm_decoder is
   port (
+  clk		:in std_logic;
 	din    		: in  STD_LOGIC_VECTOR(7 downto 0);
 	op			: in  STD_LOGIC_VECTOR(3 downto 0);
 	LM_SM       : out STD_LOGIC_VECTOR(1 downto 0);
@@ -203,7 +204,9 @@ component controlblock_MEM is
 	Mux_MEM_PC_Control : out std_logic ;
 	Mem_wr_en			: out std_logic; 
 	adw					: in std_logic_vector(2 downto 0);
-	dont_care_mem		: in std_logic
+	LMSM 					: in std_logic_vector(1 downto 0)
+
+
   ) ;
 end component;
 
@@ -214,9 +217,7 @@ component controlblock_EX is
 		cmp: in std_logic;
 		mux_ex_pc_imm_control:out std_logic_vector(1 downto 0);
 		mux_ex_alu1_control:out std_logic;
-		mux_ex_alu2_control:out std_logic_vector(1 downto 0);
-		dont_care_ex:in std_logic
-		);
+		mux_ex_alu2_control:out std_logic_vector(1 downto 0));
 end component;
 
 component wb_control is
@@ -229,8 +230,8 @@ component wb_control is
 		wr_en: out std_logic;
 		adw: in std_logic_vector(2 downto 0);
 		mux_r7_in_control : out std_logic;
-		dont_care_wb:in std_logic
-
+		valid		: in std_logic ;
+		LMSM 		: in std_logic_vector(1 downto 0)		
 	);
 end component;
  
@@ -332,12 +333,8 @@ component RR_EX is
 
 		valid_in    : in  std_logic;
 		valid_out	: out std_logic;
-
-		dont_care_ex_in :in std_logic;
-		dont_care_ex_out: out std_logic
-
-
-
+		LMSM_in			: in std_logic_vector( 1 downto 0);
+		LMSM_out			: out std_logic_vector( 1 downto 0)
 		);
 
 end component;
@@ -374,9 +371,8 @@ component EX_MEM is
 		PC_Imm_out  : out std_logic_vector(15 downto 0);
 		valid_in    : in  std_logic;
 		valid_out	: out std_logic;
-
-		dont_care_mem_in:in std_logic;
-		dont_care_mem_out:out std_logic
+				LMSM_in			: in std_logic_vector( 1 downto 0);
+		LMSM_out			: out std_logic_vector( 1 downto 0)
 	
 		);
 
@@ -411,35 +407,13 @@ component MEM_WB is
 
 		PC_Inc_out	: out std_logic_vector(15 downto 0);
 		PC_Imm_out  : out std_logic_vector(15 downto 0)	;
-		
-		dont_care_wb_in:in std_logic;
-		dont_care_wb_out:out std_logic
-
+		valid_in    : in  std_logic;
+		valid_out	: out std_logic;
+		LMSM_in			: in std_logic_vector( 1 downto 0);
+		LMSM_out			: out std_logic_vector( 1 downto 0)
+	
 		);
 
-end component;
-
-component hazard_block is 
-	port(
-		op_rr			:in std_logic_vector(3 downto 0);
-		op_ex			:in std_logic_vector(3 downto 0);
-		op_mem			:in std_logic_vector(3 downto 0);
-		ara_rr			:in std_logic_vector(2 downto 0);
-		arb_rr			:in std_logic_vector(2 downto 0);
-		adw_ex			:in std_logic_vector(2 downto 0);
-		adw_mm			:in std_logic_vector(2 downto 0);
-		aluout 			:in std_logic_vector(15 downto 0);
-		memout 			:in std_logic_vector(15 downto 0);
-		pcout 			:in std_logic_vector(15 downto 0);
-		immout 			:in std_logic_vector(15 downto 0);
-		hazard_output	:out std_logic_vector(15 downto 0);
-		mux_rr_dra_hazard_control:out std_logic;
-		mux_rr_drb_hazard_control:out std_logic;
-		disable			:out std_logic:='0';
-		dont_care_ex :out std_logic
-
-
-);
 end component;
 ---------------------------------------------------------------------
 
@@ -449,13 +423,13 @@ signal pc_out, im_ir, inc_if_id: std_logic_vector (15 downto 0) := (others => '0
 
 signal pc_pc_imm, ir_decoder, pc_inc_id, pc_imm_id, decoder_out,sign_ext_out, pc_id: std_logic_vector (15 downto 0) := (others => '0');
 
-signal  pc_imm_rr, imm_rr, rf_d1_rr, rf_d2_rr, pc_inc_rr, rr_dra_out,rr_drb_out, mux_rr_imm_dout : std_logic_vector (15 downto 0) := (others => '0');
+signal  pc_imm_rr, imm_rr, rf_d1_rr, rf_d2_rr, pc_inc_rr, rr_dra_out, mux_rr_imm_dout : std_logic_vector (15 downto 0) := (others => '0');
 
 signal lm_sm_mux_rr_adw, adw_ex, adw_mem, arb_mux_rr, adw_rr, adw_wb, lm_sm_decoder_mux_addr2, lm_sm_addr3, mux_adw_out, decoder_mux_adw, ara_addr1_rr, arb_out, mux_out_addr2, reg_address_out : std_logic_vector(2 downto 0) := (others => '0'); 
 
-signal lm_sm_rr,lm_sm_ex, lm_sm_mem, lm_sm_wb, mux_lm_sm_out, lm_sm_reg_out, lm_sm_decode_dout : std_logic_vector(7 downto 0) := (others => '0');
+signal lm_sm_rr, mux_lm_sm_out, lm_sm_reg_out, lm_sm_decode_dout : std_logic_vector(7 downto 0) := (others => '0');
 
-signal op_ex, op_rr, op_mem, op_wb : std_logic_vector(3 downto 0) := (others => '0');
+signal op_ex, op_rr, op_mem, op_wb, op_reg_out : std_logic_vector(3 downto 0) := (others => '0');
 
 signal wren_ex, pc_imm_ex, imm_ex, dra_ex,drb_ex,pc_inc_ex,aout_ex, mux_ex_alu1_out, mux_ex_alu2_out, mux_ex_pc_imm_out : std_logic_vector (15 downto 0) := (others => '0');
 
@@ -465,21 +439,19 @@ signal ren_mem, imm_mem, dra_mem, drb_mem, aout_mem, pc_inc_mem,pc_imm_mem, mux_
 
 signal imm_wb, dra_wb, drb_wb, aout_wb, mout_wb, pc_inc_wb, pc_imm_wb, mux_wb_rf_out, mux_wb_r7_out : std_logic_vector (15 downto 0) := (others => '0');
 ---enable signals
-signal en_PC, r7_en, regfile_wr_en,en_if_id,en_id_rr, en_rr_ex, en_ex_mem, en_mem_wb,reg_flag_wb_rf_en, lm_sm_main_enable_out : std_logic := '1';
+signal en_PC, r7_en, regfile_wr_en, en_rr_ex, en_ex_mem, en_mem_wb,reg_flag_wb_rf_en, lm_sm_main_enable_out : std_logic := '1';
 ---clear signals
 signal clr_PC, clr_decoder, clr_if_id, clr_id_rr, clr_rr_ex, clr_ex_mem, clr_mem_wb,clr_reg_flag_wb_rf, wren_mem : std_logic := '0';
 
 ---control signals --muxes
-signal  mux_rr_imm_control, mux_rr_addr2_rf_control, mux_rr_dra_control,mux_rr_drb_control, mux_rr_adw_control, mux_ex_alu1_control, mux_mem_pc_control, mux_mem_addr_control, mux_mem_din_control, mux_wb_pc_control, mux_wb_r7_in_control : std_logic := '0'; 
+signal  mux_rr_imm_control, mux_rr_addr2_rf_control, mux_rr_dra_control, mux_rr_adw_control, mux_ex_alu1_control, mux_mem_pc_control, mux_mem_addr_control, mux_mem_din_control, mux_wb_pc_control, mux_wb_r7_in_control : std_logic := '0'; 
 signal mux_id_adw_control, reg_flag_wb_rf_d3_control, mux_ex_pc_imm_control, mux_ex_alu2_control : std_logic_vector(1 downto 0) := (others => '0'); --mux 4 to 1
 ---lm_sm_signals
 signal lm_sm_mux_rr_dra, mux_wb_pc_out,mux_mem_pc_out  : std_logic_vector(15 downto 0) := (others => '0');
 
-signal cmp_bit, valid, valid_reg_out, clr_lm_sm_reg, control_bit, valid_ex, valid_mem,mux_rr_dra_hazard_control,disable_from_hazard,reg_flag_wb_rf_carry_en,reg_flag_wb_rf_zero_en,clr_reg_flag_wb_carry_rf,clr_reg_flag_wb_zero_rf,enable_lmsm_hazard,dont_care_hazard_block ,to_dont_care_mem_in,to_dont_care_wb_in,to_dont_care_wb_control: std_logic :='0';
+signal cmp_bit, valid, valid_reg_out, clr_lm_sm_reg, control_bit, valid_ex, valid_mem, valid_wb : std_logic :='0';
 
-signal to_lm_sm_block : std_logic_vector(1 downto 0) := (others => '0'); -- for lm_sm_decoder
-
-signal hazard_output,rr_dra_hazard_out : std_logic_vector(15 downto 0) := (others => '0');
+signal to_lm_sm_block, lm_sm_ex,lm_sm_mem,lm_sm_wb: std_logic_vector(1 downto 0) := (others => '0'); -- for lm_sm_decoder
 
 begin
 
@@ -498,8 +470,7 @@ EX_CONTROL : controlblock_EX
 		cmp 					=> cmp_bit,
 		mux_ex_pc_imm_control 	=> mux_ex_pc_imm_control,
 		mux_ex_alu1_control 	=> mux_ex_alu1_control,
-		mux_ex_alu2_control 	=> mux_ex_alu2_control,		-- needs to be connected --
-		dont_care_ex 			=> dont_care_hazard_block
+		mux_ex_alu2_control 	=> mux_ex_alu2_control 
 		);
 
 MEM_CONTROL : controlblock_MEM 
@@ -513,7 +484,7 @@ MEM_CONTROL : controlblock_MEM
 	Mux_MEM_PC_Control 		=> mux_mem_pc_control,
 	Mem_wr_en			   => wren_mem,
 	adw					   => adw_mem,
-	dont_care_mem			=> to_dont_care_mem_in-- need to be connected
+	LMSM						=> lm_sm_mem
   );	
 
 
@@ -527,7 +498,8 @@ port map(
 	wr_en 				   => regfile_wr_en,
 	adw 				   => adw_wb,
 	mux_r7_in_control      => mux_wb_r7_in_control,
-	dont_care_wb		=> to_dont_care_wb_control--- need to be connected ---
+	valid					=> valid,
+	LMSM 					=> to_lm_sm_block
 );
 
 
@@ -566,14 +538,14 @@ IF_ID_REG : IF_ID
 		clk			=> clk,
 		clr			=> clr_if_id,
 		reset       => reset,
-		en 			=> en_if_id,
+		en 			=> lm_sm_main_enable_out,
 
 		pc_in 		=> pc_out,
 		ir_in 		=> im_ir,
 		pc_inc_in	=> inc_if_id,
 	------------=> ,----------------
 		pc_out 		=> pc_pc_imm,
-		ir_out		=> ir_decoder,
+		ir_out		=> decoder_out,
 		pc_inc_out	=> pc_id
 		);
 
@@ -596,15 +568,12 @@ PC_IMMIDIATE : adder
         );
 
 
-DECODER : instr_decoder   
-  port map(
-  	   clock  	=> clk,
-  	   reset 	=> reset,
-  	   clr    => clr_decoder,
-  	
-       din   	=> ir_decoder,  
-       dout     => decoder_out
-      );
+--DECODER : instr_decoder   
+--  port map(
+--  	   clock  	=> clk,
+--  	   reset 	=> reset,ir
+--       dout     => decoder_out
+--      );
 
 MUX_ID_ADW : mux4to1 
 	generic map(3)
@@ -636,12 +605,22 @@ lm_sm_reg : my_reg
 		din     => mux_lm_sm_out,
 		dout    => lm_sm_reg_out
 		);
-
+op_reg_lm_sm : my_reg
+	generic map(4)
+	port map(
+		clk 		=> clk,
+		en 		=> '1',
+		clr  		=> '0',
+		reset   => reset,
+		din     => decoder_out(15 downto 12),
+		dout    => op_reg_out
+		);
 
 LM_SM_DECODE : lm_sm_decoder --addded
   port map (
+  clk				=> clk,
 	din    		=> lm_sm_reg_out,
-	op			=> decoder_out(15 downto 12),
+	op				=> op_reg_out,
 	LM_SM       => to_lm_sm_block,
     dout   		=> lm_sm_decode_dout,
     Reg_Address => reg_address_out,
@@ -667,7 +646,7 @@ LM_SM_MAIN : LM_SM_Block
 -------------------------------------------------------------------------
 adw_control : id_adw_control
   port map (
-	op	  				=> ir_decoder(15 downto 12),
+	op	  				=> decoder_out(15 downto 12),
 	id_adw_mux_control  => mux_id_adw_control
   ); 
 
@@ -678,7 +657,7 @@ ID_RR_REG : ID_RR
 		clk			=> clk,
 		clr 		=> clr_id_rr,
 		reset 		=> reset,
-		en 			=> en_id_rr,
+		en 			=> lm_sm_main_enable_out,
 		pc_imm_in	=> pc_imm_id,
 		imm_in  	=> sign_ext_out,
 		ara_in		=> decoder_out(11 downto 9),
@@ -738,34 +717,13 @@ ID_RR_REG : ID_RR
 	reset       	=> reset
     );
 
-    -- first mux --
- Mux_RR_DRA_Hazard: mux2to1 
+ Mux_RR_DRA: mux2to1 
 	generic map(16)
 	port map(
 		d0 		=>  rf_d1_rr,
-		d1  	=>  hazard_output,
-	  	sel		=>  mux_rr_dra_hazard_control,
-		dout 	=>  rr_dra_hazard_out
-	);
-
--- second mux --
- Mux_RR_DRA_LMSM: mux2to1 
-	generic map(16)
-	port map(
-		d0 		=>  rr_dra_hazard_out,
 		d1  	=>  aout_ex,
-	  	sel		=>  mux_rr_dra_control, -- mux_rr_dra_LMSM_control
+	  	sel		=>  mux_rr_dra_control,
 		dout 	=>  rr_dra_out
-	);
-
---------------------------------------------------------- added for hazard ------------------------------------------------------------
- Mux_RR_DRB: mux2to1 
-	generic map(16)
-	port map(
-		d0 		=>  rf_d2_rr,
-		d1  	=>  hazard_output,
-	  	sel		=>  mux_rr_drb_control,
-		dout 	=>  rr_drb_out
 	);
 
  Mux_RR_ADW: mux2to1 
@@ -789,7 +747,7 @@ port map (
 		Imm_in 		 => imm_rr,
 		OP_in 		 => op_rr,
 		DRA_in 		 => rr_dra_out,
-		DRB_in 		 => rr_drb_out,
+		DRB_in 		 => rf_d2_rr,
 		ADW_in 		 => mux_adw_out,
 		Type_in 	 => type_rr,
 
@@ -807,9 +765,8 @@ port map (
 		Type_out 	 => type_ex,
 
 		PC_Inc_out 	 => pc_inc_ex,
-		dont_care_ex_in=>dont_care_hazard_block,---new take from hazard
-		dont_care_ex_out=>to_dont_care_mem_in
-		
+		LMSM_in			=> to_lm_sm_block,
+		LMSM_out			=> lm_sm_ex
 		);
 
 ---------------------------------------------------------
@@ -889,9 +846,8 @@ EX_MEM_REG: EX_MEM
 
 		PC_Inc_out	 => pc_inc_mem,
 		PC_Imm_out   => pc_imm_mem,
-		
-		dont_care_mem_in=>to_dont_care_mem_in,
-		dont_care_mem_out=>to_dont_care_wb_in
+		LMSM_in		=> lm_sm_ex,
+		LMSM_out		=> lm_sm_mem
 		);
 	
 Mux_MEM_PC : mux2to1
@@ -961,10 +917,11 @@ MEM_WB_REG: MEM_WB
 		Type_out	 => type_wb,
 
 		PC_Inc_out	 => pc_inc_wb,
-		PC_Imm_out   => pc_imm_wb ,
-
-		dont_care_wb_in=>to_dont_care_wb_in,
-		dont_care_wb_out=>to_dont_care_wb_control
+		PC_Imm_out   => pc_imm_wb,
+			valid_in   => valid_mem,
+			valid_out  => valid_wb,
+				LMSM_in		=> lm_sm_mem,
+		LMSM_out		=> lm_sm_wb
 		);
 
 
@@ -997,30 +954,17 @@ MUX_WB_R7_IN: mux2to1
 		dout 	=> mux_wb_r7_out 
 	);
 
-REG_FLAG_WB_RF_CARRY: my_reg_one
+REG_FLAG_WB_RF: my_reg
+	generic map (2)
 	port map
 	(
 		clk 	=> clk,
-		en 		=> reg_flag_wb_rf_carry_en,
-		clr  	=> clr_reg_flag_wb_carry_rf,
+		en 		=> reg_flag_wb_rf_en,
+		clr  	=> clr_reg_flag_wb_rf,
 		reset 	=> reset,
-		din  	=> flags_wb(1),
-		dout 	=> reg_flag_wb_rf_out(1)
+		din  	=> flags_wb,
+		dout 	=> reg_flag_wb_rf_out
 	);
-
-REG_FLAG_WB_RF_ZERO: my_reg_one
-	port map
-	(
-		clk 	=> clk,
-		en 		=> reg_flag_wb_rf_zero_en,
-		clr  	=> clr_reg_flag_wb_zero_rf,
-		reset 	=> reset,
-		din  	=> flags_wb(0),
-		dout 	=> reg_flag_wb_rf_out(0)
-	);
-
-	
-
 --- i don't know whether it will work
 zero_flag : process(mem_dout, op_mem, flags_mem1)
 	begin
@@ -1032,77 +976,13 @@ zero_flag : process(mem_dout, op_mem, flags_mem1)
 		end if;	
 	end process ; -- identifier	
 
---------------------------------------------------------------------------------hazard 
-
---hazard_block_instance : hazard_block
---	port map(
---		op_rr						=> decoder_out(15 downto 12),
---		op_ex						=> op_rr,
---		op_mem						=> op_ex,
---		ara_rr						=> decoder_out(11 downto 9),
---		arb_rr						=> decoder_out(8 downto 6),
---		adw_ex						=> ara_addr1_rr,
---		adw_mm						=> adw_ex,
---		aluout 						=> aout_ex,
---		memout 						=> mem_dout,	
---		pcout 						=> pc_inc_rr,	--pcout stands for pc++ --
---		immout 						=> imm_rr,	
---		hazard_output				=> hazard_output,		
---		mux_rr_dra_hazard_control	=> mux_rr_dra_hazard_control,
---		mux_rr_drb_hazard_control	=> mux_rr_drb_control,
---		disable						=> disable_from_hazard,
---		dont_care_ex				=> dont_care_hazard_block);
-
-
-
-hazard_block_instance : hazard_block
-	port map(
-		op_rr						=> op_rr,
-		op_ex						=> op_ex,
-		op_mem						=> op_mem,
-		ara_rr						=> ara_addr1_rr,
-		arb_rr						=> mux_out_addr2,
-		adw_ex						=> adw_ex,
-		adw_mm						=> adw_mem,
-		aluout 						=> aout_ex,
-		memout 						=> mem_dout,	
-		pcout 						=> pc_inc_rr,	--pcout stands for pc++ --
-		immout 						=> imm_ex,	
-		hazard_output				=> hazard_output,		
-		mux_rr_dra_hazard_control	=> mux_rr_dra_hazard_control,
-		mux_rr_drb_hazard_control	=> mux_rr_drb_control,
-		disable						=> disable_from_hazard,
-		dont_care_ex				=> dont_care_hazard_block);
- --LM_SM Block is missing. Needs to be entered
-
-flag_ex_proc : process( op_ex, flags_ex )
-begin
-case (op_ex) is
-	when "0000" | "0001"=>
-		reg_flag_wb_rf_carry_en	<= '1' ;
-		reg_flag_wb_rf_zero_en  <= '1' ;
-	
-	when "0010" | "0100" =>
-		reg_flag_wb_rf_carry_en	<= '0' ;
-		reg_flag_wb_rf_zero_en  <= '1' ;
-
-	when others =>
-		reg_flag_wb_rf_carry_en	<= '0' ;
-		reg_flag_wb_rf_zero_en  <= '0' ;
-end case;
-end process ; 
-
-
 -------asigning now may change later --------------------
-enable_lmsm_hazard <= lm_sm_main_enable_out or not disable_from_hazard ;
-en_PC 					<= enable_lmsm_hazard;
---en_PC 					<= '1';
+en_PC 					<= '1';
 r7_en 					<= '1';
-en_if_id				<=	enable_lmsm_hazard ;
-en_id_rr				<=  enable_lmsm_hazard ;
 en_rr_ex					<= '1';
 en_ex_mem				<= '1';
 en_mem_wb				<= '1';
+reg_flag_wb_rf_en		<= '1';
 clr_PC					<= '0';
 clr_decoder				<= '0';
 clr_if_id				<= '0';
@@ -1110,13 +990,10 @@ clr_id_rr				<= '0';
 clr_rr_ex				<= '0';
 clr_ex_mem				<= '0';
 clr_mem_wb				<= '0';
+clr_reg_flag_wb_rf	<= '0';
 clr_lm_sm_reg			<= '0';
- 
-clr_reg_flag_wb_zero_rf	<= '0' ;
-clr_reg_flag_wb_carry_rf	<= '0' ;
 
 ----------------------------------------------------------	
 
-
+-- LM_SM Block is missing. Needs to be entered
 end arch;
-
